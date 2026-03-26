cdd6dc9f3d3715445794756a176d3ceb1dc1670b
冯圳宇 <11710294309@qq.com>
2026-03-26 13:31:21 +0800

    chore: set vite base for github pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -11,7 +11,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RM_Tactics_Board/',
+  base: '/RCIA_Tactics_Board/',
   plugins: [
     VueRouter(),
     Vue({
